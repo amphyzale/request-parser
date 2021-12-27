45925d040aea2599f4191a23810fe9e066de7837
Pyanov Alexandr <alexandr.pyanov@bsc-ideas.com>
2021-12-27 20:14:57 +0300

    add validation

--- src/main/java/com/curseproject/alex/requestparser/controller/MainController.java
+++ src/main/java/com/curseproject/alex/requestparser/controller/MainController.java
@@ -4,13 +4,16 @@ import com.curseproject.alex.requestparser.model.Info;
 import com.curseproject.alex.requestparser.model.Response;
 import com.curseproject.alex.requestparser.service.InfoProcessorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
-
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/request_parser/api/v1")
@@ -19,7 +22,7 @@ public class MainController {
     private final InfoProcessorService service;
 
     @PostMapping("/from_json")
-    public Response info(@RequestBody @Valid List<Info> request) {
+    public Response info(@RequestBody @NotEmpty @Size(min=1) List<@Valid Info> request) {
         return service.processRequest(request);
     }
 
